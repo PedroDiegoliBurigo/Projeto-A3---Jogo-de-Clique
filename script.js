0ed91ad1ce89b05de4111513eef1669a83fa9592
Pedro Henrique Diegoli Burigo <pedro.burigo@unifebe.edu.br>
2025-06-17 15:28:55 -0300

    poder 1 terminado

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let pontos = 0;
 let pontosPorClique = 1;
 let custoPoder1 = 10;
+let quant1 = 0;
+
+document.getElementById('custo1').innerText = 'Custo = '+ custoPoder1;
 
 function aumentarPontos () {
     pontos += pontosPorClique;
@@ -11,10 +14,12 @@ function poder1 () {
     if ( pontos >= custoPoder1) {
         pontos -= custoPoder1;
         pontosPorClique += 1;
+        quant1 += 1;
         document.getElementById('mostraPontos').innerText = "Pontos = " + pontos ;
-        custoPoder1 = Math.floor(custoPoder1 * 1.5);
+        custoPoder1 = Math.floor(custoPoder1 * 1.2);
         document.getElementById('custo1').innerText = 'Custo = '+ custoPoder1;
         document.getElementById('poderClique').innerText = 'Pontos por Clique = '+ pontosPorClique;
+        document.getElementById('quanto1').innerText =' Pode1 = ' + quant1;
         
     } else {
         alert ('Pontos insuficientes');
