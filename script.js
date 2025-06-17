fce21ce26e7d90ecacb9866a378248e06fb61d47
Pedro Henrique Diegoli Burigo <pedro.burigo@unifebe.edu.br>
2025-06-17 15:10:22 -0300

    primeiro poder

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let pontos = 0;
+let pontosPorClique = 1;
+let custoPoder1 = 10;
 
 function aumentarPontos () {
-    pontos ++;
+    pontos += pontosPorClique;
     document.getElementById('mostraPontos').innerText = "Pontos = " + pontos ;
 }
+
+function poder1 () {
+    if ( pontos >= custoPoder1) {
+        pontos -= custoPoder1;
+        pontosPorClique += 1;
+        document.getElementById('mostraPontos').innerText = "Pontos = " + pontos ;
+    } else {
+        alert ('Pontos insuficientes');
+    }
+        
+}
+
